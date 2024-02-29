807ddc09e912f7e77b2a74b8d19397989f6bfaca
Manya-pt <145551054+Manya-pt@users.noreply.github.com>
2024-02-29 16:30:56 +0530

    Add files via upload

---
+++ BoxDemo5.java
@@ -0,0 +1,34 @@
+class Box
+{
+	double depth;
+	double height;
+	double width;
+	double volume()
+	{
+	 return width*height*depth;
+	}
+	void setDim(double w,double h,double d)
+	{
+		width=w;
+		height=h;
+		depth=d;
+		}
+}
+class BoxDemo5
+{
+	public static void main(String args[])
+	{
+		Box mybox1=new Box();
+		Box mybox2=new Box();
+		
+		 double vol ;
+		 mybox1.setDim(10,20,5);
+		
+		mybox2.setDim(3,6,9);
+		vol=mybox1.volume();
+		System.out.println("volume is"+ vol);
+		vol=mybox2.volume();
+		System.out.println("volume is"+ vol);
+		}
+}
+
